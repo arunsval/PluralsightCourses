12448708cf92fbf7f5f14212cdfab4de374e4baf
Arun Selvam P M <arun666selvam@gmail.com>
2023-03-07 00:10:46 +0530

    Updated openweather findings

--- openweathermap.py
+++ openweathermap.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_weather_status(latitude, longitude):
-    api_key = "b0e5a414003fe3e5899660e8900e0a0b"
+    api_key = ""
     latitude = 80
     longitude = 100
     url = "https://api.openweathermap.org/data/2.5/weather?lat="+str(latitude)+"&lon="+str(longitude)+"&appid="+api_key
